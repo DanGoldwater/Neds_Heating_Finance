10ac73dda21323660e75a993446fb5d6a77133da
DanGoldwater <dangoldwater@gmail.com>
2022-03-18 21:44:56 +0000

    started move to objects

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #%%
+from dataclasses import dataclass
 import numpy_financial as npf
 
 mortgage_1 = {
@@ -7,6 +8,35 @@ mortgage_1 = {
     'loan': 50000
 }
 
+@dataclass
+class scenario:
+    loan_years: int
+    rate: int
+    loan_amount: int
+    electricity_price: float
+    heating_need: int
+    
+    def monthly_loan_payment(self):
+        int_per_month = self.rate / (100 * 12)
+        return - npf.pmt(int_per_month, 12 * self.loan_years, self.loan_amount)
+
+    def monthly_heat_source_bill(self):
+        COP_air_source = 3.4
+        return (self.heating_need / COP_air_source) * self.electricity_price
+
+    def monthly_ordinary_heating_bill(self):
+        return self.heating_need * self.electricity_price 
+
+    def monthly_air_total(self):
+        return self.monthly_loan_payment() + self.monthly_heat_source_bill()
+
+    def give_summary(self):
+        print(f'Monthly loan payments would be £{self.monthly_loan_payment() } ')
+        print(f'Monthly heating bill would be £{self.monthly_heat_source_bill()} ')
+        print(f'Monthly total bill would be {self.monthly_air_total()}')
+        print(f'\n Or, with just electric heating:')
+        print(f'Monthly heating bill would be £{self.monthly_ordinary_heating_bill() } ')
+    
 
 
 
@@ -32,8 +62,15 @@ def give_summary(scenario):
     print(f'Monthly heating bill would be £{ordinary_bill} ')
     
 
+scen_1 = {
+    'loan_years': 25,
+    'rate': 6,
+    'loan_amount': 40_000,
+    'electricity_price': .3,
+    'heating_need': 25000
+}
     
-  
-
+scennn_1 = scenario(**scen_1) 
+scennn_1.give_summary()
     
 
